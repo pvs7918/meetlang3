cc9c808952b18f1f9e1b71d8e1c2cf54943ef7cd
Pavel <p_v_s@mail.ru>
2022-08-13 15:49:05 +0700

    Переписал зада 21 с использование функции

--- urok_task21/Program.cs
+++ urok_task21/Program.cs
@@ -3,6 +3,18 @@ A (3,6); B (2,1) -> 5,09
 A (7,-5); B (1,-1) -> 7,21
 */
 
+double FindDist(double x1, double y1, double x2, double y2)
+{
+    double res;
+    double dx;
+    double dy;
+
+    dx=Math.Abs(x1-x2);
+    dy=Math.Abs(y1-y2);
+    res = Math.Sqrt(Math.Pow(dx,2)+Math.Pow(dy,2));
+    return res;
+}
+
 Console.Write("x1:");
 double x1 = double.Parse(Console.ReadLine()!);
 Console.Write("y1:");
@@ -12,12 +24,4 @@ double x2 = double.Parse(Console.ReadLine()!);
 Console.Write("y2:");
 double y2 = double.Parse(Console.ReadLine()!);
 
-double dist;
-double dx;
-double dy;
-
-dx=Math.Abs(x1-x2);
-dy=Math.Abs(y1-y2);
-dist = Math.Sqrt(Math.Pow(dx,2)+Math.Pow(dy,2));
-
-Console.Write($"Расстояние равно: {dist}");
+Console.Write($"Расстояние равно: {FindDist(x1, y1, x2, y2)}");
