be2c5f6a21ceb02bf578626cef62d2e500cb56c6
Pavel <p_v_s@mail.ru>
2022-08-13 14:57:10 +0700

    Добалвена Задача 18 на уроке. первый коммит

---
+++ urok_task18/Program.cs
@@ -0,0 +1,28 @@
+﻿string PrintXYGradeOfQuater(int quater)
+{
+    string res;
+    switch(quater)
+    {
+        case 1:
+            res = "X > 0, Y > 0";
+            break;
+        case 2:
+            res = "X < 0, Y > 0";
+            break;
+        case 3:
+            res = "X > 0, Y < 0";
+            break;
+        case 4:
+            res = "X < 0, Y < 0";
+            break;    
+        default:
+            res = "Указан неверный номер четверти!";
+            break;
+    }
+    return res;
+}
+
+Console.Write("Введите номер четверти: ");
+int q = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"{PrintXYGradeOfQuater(q)}");
